695c251732cf8039fe2f75c21a1926549aa0b108
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-03-23 03:13:10 +0600

    connect mongoose database

---
+++ vehicle/src/lib/connectDB.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const mongoDBUrl = process.env.MONGODB_URL;
+if(!mongoDBUrl){
+    throw new Error("bd error")
+}
+let cached = global.mongoose
+
+if(!cached){
+    cached = global.mongoose={conn:null, promise:null}
+
+}
+const connectDb = async()=>{
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise)
+        cached.promise = mongoose.connect(mongoDBUrl).then((conn)=>conn.connection)
+    
+    try{
+const conn = await cached.promise
+return conn
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+export default connectDb
