92a6dbd477c349e501c205ad0dc4f1aa0c269d12
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-03-23 02:32:09 +0600

    setup next js

---
+++ vehicle/src/model/user.model.ts
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+
+interface IUser{
+    id:mongoose.Types.ObjectId;
+    name:string;
+    email:string;
+    password?:string;
+    phone:string;
+    image?:string;
+    role:"user"|"vendor"|"admin";
+    isApproved?:boolean;
+    verificationStatus:"pending"|"approved"|"rejected"
+    requestedAt:Date;
+    approvedAt:Date;
+    rejectedReason?:string;
+    vendorProduct?:mongoose.Types.ObjectId[];
+    orders?:mongoose.Types.ObjectId[];
+    cart?:{
+    product:mongoose.Types.ObjectId;
+        quantity:number
+    }[];
+    createdAt?:Date;
+    updatedAt?:Date;
+
+
+}
+
+
+
+
+const userSchema = new mongoose.Schema<IUser>({
+
+    name:{
+    type:String,
+    required:true
+},
+    email:{
+    type:String,
+    required:true
+},
+    password:{
+    type:String,
+    // required:true
+},
+image:{
+    type:String
+},
+phone:{
+    type:String,
+},
+role:{
+    type:String,
+    enum:["user","vendor","admin"],
+    default:"user"
+},
+isApproved:{
+    default:false
+},
+verificationStatus:{
+    enum:["pending","approved","rejected"],
+    default:"pending"
+},
+approvedAt:{
+    type:Date
+},
+requestedAt:{
+    type:Date
+},
+rejectedReason:{
+    type:String,
+
+},
+vendorProduct:[{
+    type:mongoose.Types.ObjectId,
+ref:'product'
+}
+],
+orders:[{
+    type:mongoose.Types.ObjectId,
+ref:'orders'
+}
+],
+cart:[
+    {
+        product:{
+             type:mongoose.Types.ObjectId,
+ref:'product'
+        },
+        quantity:{
+            type:Number,
+            default:1,
+        }
+    }
+]
+
+},{timestamps:true})
+
+const User =mongoose.models?.User || mongoose.model<IUser>("User",userSchema)
+export default User
