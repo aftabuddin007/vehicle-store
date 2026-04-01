54ba5b3745668c6472288c9ed8c66f232ff5412b
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-04-01 23:51:34 +0600

    add padding

--- vehicle/src/component/Vendor/Dashboard.tsx
+++ vehicle/src/component/Vendor/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DashboardVendor = () => {
     return (
-       <div className='flex items-center justify-center h-screen w-full'>
+       <div className='flex items-center justify-center p-7 h-screen w-full'>
             <h1 className="text-white">vendor dashboard</h1>
             
         </div>
